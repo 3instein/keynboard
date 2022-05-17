65979673b384a6453b6a28fda99922253ffe6207
ssabrut <michael.gunawan2002@gmail.com>
2022-05-17 11:44:32 +0700

    Fixed tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,10 @@
 <x-app-layout>
 
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-bold text-red-900">
         Hello world!
     </h1>
   
-    {{-- <div class="header">
-        <img src="images/gold-logo.png" alt="Key&Board Logo"
-            width=10% class="object-center">
-    </div> --}}
+    <div class="header grid place-items-center">
+        <img src="images/gold-logo.png" alt="Key&Board Logo" />
+    </div>
 </x-app-layout>
