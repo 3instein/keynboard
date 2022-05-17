243287667f8ddf5bad49546a94d4363bad7a3eb1
ssabrut <michael.gunawan2002@gmail.com>
2022-05-17 20:30:04 +0700

    Added spesification section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,13 +29,14 @@
 
   {{-- about us --}}
   <section name="about-us">
-    <div class="text-center py-20 bg-slate-100 bg-opacity-20">
+    <div class="text-center py-20 bg-slate-100 bg-opacity-30">
       <div class="grid place-items-center">
         <div class="border-b-2 border-b-base">
           <p class="text-3xl uppercase text-base tracking-widest font-extrabold mb-4">About us</p>
         </div>
         <div class="w-5/12 mt-8">
-          <p class="text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis urna eu
+          <p class="text-base">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis urna eu
             semper dictum. Aenean
             feugiat maximus nisl, id condimentum tellus sodales sit amet. Integer in molestie sapien, ut pharetra lorem.
             Pellentesque laoreet ipsum nec vulputate porttitor. Aliquam ut molestie ex, eget vehicula lectus. Donec
@@ -51,6 +52,58 @@
 
   {{-- spesification section --}}
   <section name="spesification">
+    <div class="grid place-items-center my-40">
+      <div class="flex items-center justify-center">
+        <div class="mr-32">
+          <img class="h-auto w-96" src="/images/keeb1.png" alt="keeb1">
+        </div>
+        <div class="w-1/3">
+          <div class="border-b-2 border-b-base w-fit">
+            <p class="text-3xl uppercase text-base tracking-widest font-extrabold mb-4">Keyboard spesification</p>
+          </div>
+          <div class="mt-8">
+            <p class="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis urna eu
+              semper dictum. Aenean
+              feugiat maximus nisl, id condimentum tellus sodales sit amet. Integer in molestie sapien, ut pharetra
+              lorem.
+              Pellentesque laoreet ipsum nec vulputate porttitor. Aliquam ut molestie ex, eget vehicula lectus. Donec
+              condimentum risus ut faucibus molestie. Orci varius natoque penatibus et magnis dis parturient montes,
+              nascetur ridiculus mus. Vestibulum a elit at nisl consectetur rutrum. Etiam cursus lobortis odio porttitor
+              placerat. Pellentesque sed vulputate arcu. Ut condimentum sed enim pharetra iaculis. Fusce finibus nulla
+              nulla, ac ultrices augue egestas nec. Aenean sollicitudin justo ac dolor efficitur ullamcorper.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center justify-center mt-40">
+        <div class="w-1/3">
+          <div class="border-b-2 border-b-base w-fit">
+            <p class="text-3xl uppercase text-base tracking-widest font-extrabold mb-4">Choice of materials</p>
+          </div>
+          <div class="mt-8">
+            <p class="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis urna eu
+              semper dictum. Aenean
+              feugiat maximus nisl, id condimentum tellus sodales sit amet. Integer in molestie sapien, ut pharetra
+              lorem.
+              Pellentesque laoreet ipsum nec vulputate porttitor. Aliquam ut molestie ex, eget vehicula lectus. Donec
+              condimentum risus ut faucibus molestie. Orci varius natoque penatibus et magnis dis parturient montes,
+              nascetur ridiculus mus. Vestibulum a elit at nisl consectetur rutrum. Etiam cursus lobortis odio porttitor
+              placerat. Pellentesque sed vulputate arcu. Ut condimentum sed enim pharetra iaculis. Fusce finibus nulla
+              nulla, ac ultrices augue egestas nec. Aenean sollicitudin justo ac dolor efficitur ullamcorper.
+            </p>
+          </div>
+        </div>
+        <div class="ml-32">
+          <img class="h-auto w-96" src="/images/keeb1.png" alt="keeb1">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  {{-- how to section --}}
+  <section name="how-to">
 
   </section>
 </x-app-layout>
